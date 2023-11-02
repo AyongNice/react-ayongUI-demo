ae730305efffb9f5a31c52e1323f7972fe63de64
AyongNice <AyongNice@163.com>
2023-11-02 20:20:33 +0800

    button demo  添加示例 1 、设置防抖 1000毫秒 2 、大小 size 设置 3 、自定义 className 样式

--- src/page/button/button.tsx
+++ src/page/button/button.tsx
@@ -6,10 +6,30 @@ import './index.less'
 
 const ButtonPage = () => {
 
+    const onClick = () => {
+        console.log(111)
+    }
+
     return (
         <div>
-            <h1>button</h1>
-            <Button type="primary" color='#fff' size='large'>PrimaryButton</Button>
+            <h1 onClick={onClick}>button</h1>
+
+            <fieldset>
+                <legend>大小 size 设置</legend>
+                <Button type="primary" className='left' size='small'>small-ayongUI</Button>
+                <Button type="primary" className='left' size='default'>default-ayongUI</Button>
+                <Button type="primary" className='left' size='large'>large-ayongUI</Button>
+            </fieldset>
+
+            <fieldset>
+                <legend>自定义 className 样式</legend>
+                <Button type="primary" onClick={onClick} className='diy'>small-ayongUI</Button>
+            </fieldset>
+
+            <fieldset>
+                <legend>设置防抖 1000毫秒</legend>
+                <Button type="primary" onClick={onClick} time={1000}>small-ayongUI</Button>
+            </fieldset>
         </div>
     )
 }
