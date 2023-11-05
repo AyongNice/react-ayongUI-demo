7d237286ae26faef579578d6ffd539cc4b89ecb1
楸枰 <5331659+gslqiuping@user.noreply.gitee.com>
2023-11-05 18:53:43 +0800

    1 界面样式优化

--- src/page/icon/icon.tsx
+++ src/page/icon/icon.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
+import { StepBackwardOutlined } from "@ant-design/icons";
+import "./index.less";
+import { Alipay, Alert, PreviousStep } from "../../ayongUI/index.ts";
 
 const Icon = () => {
-    return (
-        <div>
-            <h1>Icon</h1>
-        </div>
-    )
-}
-export default Icon
+  return (
+    <div>
+      <h1>Icon</h1>
+      <Alipay className="icon" style={{ color: "#ff8e4a" }} />
+      <Alert />
+      <PreviousStep />
+    </div>
+  );
+};
+export default Icon;
