ca214d01ae02b7733b9718c8e8ca3a7dbfff844c
AyongNice <AyongNice@163.com>
2023-11-05 16:55:28 +0800

    1 界面样式优化

--- src/page/icon/icon.tsx
+++ src/page/icon/icon.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import {StepBackwardOutlined} from '@ant-design/icons';
+import './index.less'
+import {Alipay, Alert} from "../../ayongUI/index.ts";
 
 const Icon = () => {
     return (
         <div>
             <h1>Icon</h1>
+            <Alipay className='icon' style={{color: '#ff8e4a'}}/>
+            <Alert/>
+
         </div>
     )
 }
