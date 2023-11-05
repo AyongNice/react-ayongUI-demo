e8afc23130ed8795433a15f4f7007ecf014927b3
楸枰 <5331659+gslqiuping@user.noreply.gitee.com>
2023-11-05 19:03:00 +0800

    添加PreviousStep图标

--- src/page/icon/icon.tsx
+++ src/page/icon/icon.tsx
@@ -14,21 +14,3 @@ const Icon = () => {
   );
 };
 export default Icon;
-=======
-import React from 'react';
-import {StepBackwardOutlined} from '@ant-design/icons';
-import './index.less'
-import {Alipay, Alert} from "../../ayongUI/index.ts";
-
-const Icon = () => {
-    return (
-        <div>
-            <h1>Icon</h1>
-            <Alipay className='icon' style={{color: '#ff8e4a'}}/>
-            <Alert/>
-
-        </div>
-    )
-}
-export default Icon
->>>>>>> 6585deb5337c9ad692ee907fd4d6212364d994b0
