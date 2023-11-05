a5e0fcaa1b35966bf62248181c6e54ff88d435e8
赵信勇 <89636513+AyongNice@users.noreply.github.com>
2023-11-05 19:43:01 +0800

    Delete src directory
    删除 demo 错误提交ayongU组件包

--- src/page/icon/icon.tsx
+++ src/page/icon/icon.tsx
@@ -1,33 +1,15 @@
+import React from "react";
+import { StepBackwardOutlined } from "@ant-design/icons";
 import "./index.less";
 import { Alipay, Alert, PreviousStep } from "../../ayongUI/index.ts";
 
-import Tab from "./components/tab.tsx";
-
 const Icon = () => {
-  let tabData = [
-    {
-      title: "线框风格",
-      content: (
-        <div>
-          <Alipay className="icon" style={{ color: "#ff8e4a" }} />
-          <Alert />
-          <PreviousStep />
-        </div>
-      ),
-    },
-    {
-      title: "实底风格",
-      content: <div>111</div>,
-    },
-    {
-      title: "双色风格",
-      content: <div>111</div>,
-    },
-  ];
   return (
     <div>
       <h1>Icon</h1>
-      <Tab defaultActiveTab={0} tabs={tabData} />
+      <Alipay className="icon" style={{ color: "#ff8e4a" }} />
+      <Alert />
+      <PreviousStep />
     </div>
   );
 };
