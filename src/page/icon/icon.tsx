f76fe527901cad58afbad200447e19abbf871669
AyongNice <AyongNice@163.com>
2023-11-05 16:55:28 +0800

    1 界面样式优化

--- src/page/icon/icon.tsx
+++ src/page/icon/icon.tsx
@@ -1,34 +1,10 @@
-import "./index.less";
-import { Alipay, Alert, PreviousStep } from "../../ayongUI/index.ts";
-
-import Tab from "./components/tab.tsx";
+import React from 'react';
 
 const Icon = () => {
-  let tabData = [
-    {
-      title: "线框风格",
-      content: (
+    return (
         <div>
-          <Alipay className="icon" style={{ color: "#ff8e4a" }} />
-          <Alert />
-          <PreviousStep />
+            <h1>Icon</h1>
         </div>
-      ),
-    },
-    {
-      title: "实底风格",
-      content: <div>111</div>,
-    },
-    {
-      title: "双色风格",
-      content: <div>111</div>,
-    },
-  ];
-  return (
-    <div>
-      <h1>Icon</h1>
-      <Tab defaultActiveTab={0} tabs={tabData} />
-    </div>
-  );
-};
-export default Icon;
+    )
+}
+export default Icon
