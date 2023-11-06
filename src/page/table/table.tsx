d6178bc51bfabf89f2992969b458bd53aedd9150
AyongNice <AyongNice@163.com>
2023-11-06 19:39:38 +0800

    1 添加 table样式演示 less文件 2 新增 md文档转换html库 3 删除 demo lock.json文件

--- src/page/table/table.tsx
+++ src/page/table/table.tsx
@@ -1,11 +1,74 @@
 import React from "react";
+import {Table} from "../../ayongUI/index.ts";
+import './index.less'
 
+const Column = Table.Column;
 
-const TablePage = () => {
+const data = [
+    {
+        key: '1',
+        age: 32,
+        name: 'John Brown',
+        address: 'New York No. 1 Lake Park',
+        tags: ['nice', 'developer'],
+    },
+    {
+        key: '2',
+        age: 42,
+        name: 'Jim Green',
+        address: 'London No. 1 Lake Park',
+        tags: ['loser'],
+    },
+    {
+        key: '3',
+        age: 32,
+        name: 'Joe Black',
+        address: 'Sydney No. 1 Lake Park',
+        tags: ['cool', 'teacher'],
+    },
+];
+const columns = [
+    {
+        title: '姓名',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: '年龄',
+        dataIndex: 'age',
+        key: 'age',
+    },
+    {
+        title: '住址',
+        dataIndex: 'address',
+        key: 'address',
+    },
+];
+
+function TablePage() {
     return (
         <div>
-            <h1>Table</h1>
+            <h1>Table Example</h1>
+            <Table className='diy-table' columns={columns} data={data}/>
+
+            <Table data={data}>
+                <Column title="Age" dataIndex="age" key={1}/>
+                <Column title="Address" dataIndex="address" key={2}/>
+                <Column
+                    key={3}
+                    title="Tags"
+                    dataIndex="tags"
+                    render={(tags: string[]) => (
+                        <>
+                            {tags.map((tag: string) => (
+                                <span style={{color: 'blue'}} key={tag}>{tag}</span>
+                            ))}
+                        </>
+                    )}
+                />
+            </Table>
         </div>
-    )
+    );
 }
-export default TablePage
+
+export default TablePage;
