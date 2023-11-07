5562875ee4ab2fae8631f6e66fe51be57507332c
sgao98 <2418965239@qq.com>
2023-11-07 21:56:40 +0800

    图标复制

--- src/page/icon/icon.tsx
+++ src/page/icon/icon.tsx
@@ -2,16 +2,49 @@ import "./index.less";
 import { Alipay, Alert, PreviousStep } from "../../ayongUI/index.ts";
 
 import Tab from "./components/tab.tsx";
+import React from "react";
 
 const Icon = () => {
+  const iconCmps = [
+    {
+      cmp: Alipay,
+      name: "Alipay",
+    },
+    {
+      cmp: Alert,
+      name: "Alert",
+    },
+    {
+      cmp: PreviousStep,
+      name: "PreviousStep",
+    },
+  ];
+
+  const iconClick = (name: string) => {
+    const textarea = document.createElement("textarea");
+    textarea.value = `<${name}/>`;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+  };
+
   let tabData = [
     {
       title: "样式一",
       content: (
         <div>
-          <Alipay className="icon" style={{ color: "#ff8e4a" }} />
-          <Alert />
-          <PreviousStep />
+          {iconCmps.map((item, index) => {
+            return (
+              <div
+                className="icon-item"
+                key={index}
+                onClick={() => iconClick(item.name)}
+              >
+                {React.createElement(item.cmp)}
+              </div>
+            );
+          })}
         </div>
       ),
     },
