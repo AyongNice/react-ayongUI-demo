88f6e1e1d0d78231e5326d8db8b64f540dca3106
WUhj7 <2275066931@qq.com>
2023-11-13 22:27:29 +0800

    加载部分icon

--- src/page/icon/icon.tsx
+++ src/page/icon/icon.tsx
@@ -1,5 +1,7 @@
 import "./index.less";
-import {Alipay, Alert, PreviousStep} from "@/ayongUI/index.ts";
+import {Alipay, Alert, PreviousStep, FileCopy, CodeDisplay, LabelersLists, UserHollow,
+    EyesOpen,EyesClosed,Setup,Collapse,Unfold
+} from "@/ayongUI/index.ts";
 
 import Tab from "./components/tab.tsx";
 import React from "react";
@@ -19,8 +21,44 @@ const Icon = () => {
             cmp: PreviousStep,
             name: "PreviousStep",
         },
+        {
+            cmp: FileCopy,
+            name: "FileCopy",
+        },
+        {
+            cmp: CodeDisplay,
+            name: "CodeDisplay",
+        },
+        {
+            cmp: LabelersLists,
+            name: "LabelersLists",
+        },
+        {
+            cmp: Collapse,
+            name: "Collapse",
+        },
+        {
+            cmp: Unfold,
+            name: "Unfold",
+        },
+        {
+            cmp: UserHollow,
+            name: "UserHollow",
+        },
+        {
+            cmp: EyesOpen,
+            name: "EyesOpen",
+        },
+        {
+            cmp: EyesClosed,
+            name: "EyesClosed",
+        },
+        {
+            cmp: Setup,
+            name: "Setup",
+        },
     ];
-    console.log(iconCmps)
+
     const iconClick = (name: string) => {
         copyText(`<${name}/>`);
     };
@@ -32,10 +70,9 @@ const Icon = () => {
                     {iconCmps.map((item, index) => {
                         return (
                             <div className="icon-border" key={index}>
-                                {React.createElement(item.cmp, {
-                                    className: 'icon-item',
-                                    onClick: () => iconClick(item.name)
-                                })}
+                                <div className="icon-item" onClick={() => iconClick(item.name)}>
+                                    {React.createElement(item.cmp)}
+                                </div>
                             </div>
                         );
                     })}
