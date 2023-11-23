2be408b310f0dc12f65f1ac451f3c6c13a2936ca
AyongNice <AyongNice@163.com>
2023-11-23 18:14:58 +0800

    1:useCallback demo 验证

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useReducer, useEffect, useContext } from 'react';
+import React, {useState, useReducer, useEffect, useContext, useCallback, useRef} from 'react';
 import './App.less';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import {Routes, Route, Navigate} from 'react-router-dom';
 
 import MenuList from './page/menu-list/menu-list.tsx';
 
@@ -12,41 +12,56 @@ import RadioPage from './page/radio/radio.tsx';
 import InputPage from './page/input/input.tsx';
 import TooltipPage from './page/tooltip/tooltip.tsx';
 
-import { Button } from './ayongUI/index.ts';
+import {Button} from './ayongUI/index.ts';
 
-import { useGlobalState } from './data-store/index.ts';
+import {useGlobalState} from './data-store/index.ts';
 
 function App() {
-	const [theme, setTheme] = useGlobalState('theme');
+    const [theme, setTheme] = useGlobalState('theme');
+    const countRef = useRef(0);
 
-	const toggleTheme = (): void => {
-		setTheme((previe): string => {
-			const res = previe === 'night' ? 'light' : 'night';
-			window.localStorage.setItem('theme', res);
-			return res;
-		});
-	};
 
-	return (
-		<div className={theme}>
-			<Button type='primary' onClick={toggleTheme}>
-				切换-主题
-			</Button>
-			<Routes>
-				{/*重定向*/}
-				<Route path='/' element={<Navigate to='/menu/button' />}></Route>
-				<Route path='/menu' element={<MenuList />}>
-					<Route path='/menu/button' element={<ButtonPage />} />
-					<Route path='/menu/table' element={<Table />} />
-					<Route path='/menu/icon' element={<Icon />} />
-					<Route path='/menu/switch' element={<SwitchPage />} />
-					<Route path='/menu/radio' element={<RadioPage />} />
-					<Route path='/menu/input' element={<InputPage />} />
-					<Route path='/menu/tooltip' element={<TooltipPage />} />
-				</Route>
-			</Routes>
-		</div>
-	);
+    const toggleTheme = useCallback((): void => {
+        if (!countRef.current) {
+            (countRef.current++);
+            return;
+        }
+        setTheme((previe): string => {
+            const res = previe === 'night' ? 'light' : 'night';
+            window.localStorage.setItem('theme', res);
+            return res;
+        });
+    }, [theme]);
+
+
+    // const toggleTheme = (): void => {
+    //     // setTheme((previe: string) => {
+    //     //     const res = previe === 'night' ? 'light' : 'night';
+    //     //     window.localStorage.setItem('theme', res);
+    //     //     return res;
+    //     // });
+    // }
+
+    return (
+        <div className={theme}>
+            <Button type='primary' onClick={toggleTheme}>
+                切换-主题
+            </Button>
+            <Routes>
+                {/*重定向*/}
+                <Route path='/' element={<Navigate to='/menu/button'/>}></Route>
+                <Route path='/menu' element={<MenuList/>}>
+                    <Route path='/menu/button' element={<ButtonPage/>}/>
+                    <Route path='/menu/table' element={<Table/>}/>
+                    <Route path='/menu/icon' element={<Icon/>}/>
+                    <Route path='/menu/switch' element={<SwitchPage/>}/>
+                    <Route path='/menu/radio' element={<RadioPage/>}/>
+                    <Route path='/menu/input' element={<InputPage/>}/>
+                    <Route path='/menu/tooltip' element={<TooltipPage/>}/>
+                </Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
